aa8bb783e60e7da01a47b72997fd8e63672537cf
nedosika <nedosika@gmail.com>
2020-07-16 15:59:24 +0300

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,9 +24,18 @@ function App() {
                 <img src={man} alt={"about"}/>
                 <h3>I am cool frontend developer</h3>
                 <p>We will evaluate how clean your approach to writing CSS and Javascript code is. You can use any CSS and Javascript 3rd party libraries without any restriction.</p>
-                <p>If 3rd party css/javascript libraries are added to the project via bower/npm/yarn you will get bonus points. If you use any task runner (gulp/webpack) you will get bonus points as well. Slice service directory page PSD mockup​in to HTML5/CSS3.</p>
+                <p>If 3rd party css/javascript libraries are added to the project via bower/npm/yarn you will get bonus points. If you use any task runner (gulp/webpack) you will get bonus points as well. Slice service directory page PSD mockup into HTML5/CSS3.</p>
                 <button>Sign up now</button>
             </section>
+            <sectiopn className={styles.users}>
+                <h2>Our cheerful users</h2>
+                <p>Attention! Sorting users by registration date</p>
+                <img alt={"user"}/>
+                <h3>Maximilian</h3>
+                <p>Leading specialist of the Control Department</p>
+                <p>controldepartment@gmail.com</p>
+                <p>+380506780324</p>
+            </sectiopn>
         </div>
     );
 }
