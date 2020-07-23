1859c9d6ed716ae2070d6703d90f296b0cabf4c6
nedosika <nedosika@gmail.com>
2020-07-23 11:38:06 +0300

    add scroll and style to current menu item

--- src/components/Header/components/TopBar/TopBar.jsx
+++ src/components/Header/components/TopBar/TopBar.jsx
@@ -1,18 +1,65 @@
 import React from "react";
+import Scrollchor from "react-scrollchor";
 
 import styles from "./topBar.module.scss";
 
 import menuIcon from "Assets/menu icon.svg";
 
 const TopBar = () => {
-    return(
+    const [current, setCurrent] = React.useState("banner");
+
+    const handleClick = (e) => {
+        setCurrent(e.target.dataset.action);
+    }
+
+    return (
         <nav className={styles.wrapper}>
-            <ul className={styles.menu}>
-                <li className={styles.menuItem}><a href={"#about"}>About me</a></li>
-                <li className={styles.menuItem}><a href={"#register"}>Relationships</a></li>
-                <li className={styles.menuItem}><a href={"#register"}>Requirements</a></li>
-                <li className={styles.menuItem}><a href={"#users"}>Users</a></li>
-                <li className={styles.menuItem}><a href={"#register"}>Sign Up</a></li>
+            <ul className={styles.menu} onClick={handleClick}>
+                <li className={styles.menuItem}>
+                    <Scrollchor
+                        to="#about"
+                        data-action={"about"}
+                        className={current === "about" && styles.action}
+                    >
+                        About me
+                    </Scrollchor>
+                </li>
+                <li className={styles.menuItem}>
+                    <Scrollchor
+                        to="#register"
+                        data-action={"relationships"}
+                        className={current === "relationships" && styles.action}
+                    >
+                        Relationships
+                    </Scrollchor>
+                </li>
+                <li className={styles.menuItem}>
+                    <Scrollchor
+                        to="#register"
+                        data-action={"requirements"}
+                        className={current === "requirements" && styles.action}
+                    >
+                        Requirements
+                    </Scrollchor>
+                </li>
+                <li className={styles.menuItem}>
+                    <Scrollchor
+                        to="#users"
+                        data-action={"users"}
+                        className={current === "users" && styles.action}
+                    >
+                        Users
+                    </Scrollchor>
+                </li>
+                <li className={styles.menuItem}>
+                    <Scrollchor
+                        to="#register"
+                        data-action={"register"}
+                        className={current === "register" && styles.action}
+                    >
+                        Sign Up
+                    </Scrollchor>
+                </li>
             </ul>
             <button><img src={menuIcon} alt="menu"/></button>
         </nav>
