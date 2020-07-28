dc5949de0b221d470273d1fa78861f0aaebf5ebe
nedosika <nedosika@gmail.com>
2020-07-28 16:35:24 +0300

    add ToolTip

--- src/components/Users/components/User/User.jsx
+++ src/components/Users/components/User/User.jsx
@@ -15,22 +15,20 @@ const User = ({className, user}) => {
     return (
         <div className={classes(className, styles.wrapper)}>
             <img className={styles.userPhoto} src={user.photo || photoCover} alt={"user"}/>
-                <ul className={styles.info}>
-                    <ToolTip text={user.name}>
-                        <li className={styles.userName}>
-                            <h2>{user.name}</h2>
-                        </li>
-                    </ToolTip>
-                    <ToolTip text={user.position}>
-                        <li className={styles.userDetail}>{user.position}</li>
-                    </ToolTip>
-                    <ToolTip text={user.email}>
-                        <li className={styles.userDetail}>{user.email}</li>
-                    </ToolTip>
-                    <ToolTip text={user.phone}>
-                        <li className={styles.userDetail}>{user.phone}</li>
-                    </ToolTip>
-                </ul>
+            <ul className={styles.info}>
+                <li className={styles.userName}>
+                    <h2>{user.name}</h2>
+                </li>
+                <li className={styles.userDetail}>{user.position}</li>
+
+                    <li className={styles.userDetail}>
+                        <ToolTip text={user.email}>
+                            <span className={styles.email}>{user.email}</span>
+                        </ToolTip>
+                    </li>
+
+                <li className={styles.userDetail}>{user.phone}</li>
+            </ul>
         </div>
     );
 }
