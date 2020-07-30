4502530b8f5e1d8a9ba3388bee383629ae8cd55a
nedosika <nedosika@gmail.com>
2020-07-30 16:16:42 +0300

    add focus

--- src/components/Register/components/Input/Input.jsx
+++ src/components/Register/components/Input/Input.jsx
@@ -3,8 +3,8 @@ import classNames from "classnames";
 
 import styles from "./Input.module.scss";
 
-const Input = ({handleChange, name, value, placeholder, error}) =>
-    <label className={styles.wrapper}>Name
+const Input = ({label, handleChange, name, value, placeholder, error}) =>
+    <label className={styles.wrapper}>{label}
         <input
             className={classNames(styles.item, error && styles.error)}
             type={"text"}
